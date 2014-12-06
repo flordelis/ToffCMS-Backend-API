551ddeb5ef98a710701bcfb57b55f351a4e1a5e8
Matiss Janis Aboltins <matiss.ja@gmail.com>
2014-12-06 16:29:32 +0200

    Gallery item tests

---
+++ app/tests/models/GalleryItemTest.php
@@ -0,0 +1,21 @@
+<?php
+
+class GalleryItemTest extends TestCase {
+
+	/**
+	 * Set the seeds
+	 */
+	public function setUp()
+	{
+		parent::setUp();
+
+		Eloquent::unguard();
+		$this->seed('GalleryTableSeeder');
+	}
+
+	public function testGallery()
+	{
+		$item = Gallery_Item::find(1);
+		$this->assertSame($item->gallery->id, Gallery::find(1)->id);
+	}
+}
