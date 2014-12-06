2ba05e93ab963648ddcd46379dbfbf95e71b0b6f
Matiss Janis Aboltins <matiss.ja@gmail.com>
2014-12-06 13:11:28 +0200

    Validation exception test

---
+++ app/tests/exceptions/ValidationExceptionTest.php
@@ -0,0 +1,15 @@
+<?php
+
+class ValidationExceptionTest extends TestCase {
+
+	public function testException()
+	{
+		try {
+			throw new ValidationException(Validator::make(array(), array()));
+		} catch (ValidationException $e) {
+			$this->assertTrue($e->get() instanceof \Illuminate\Validation\Validator);
+			$this->assertTrue(is_array($e->allMessages()));
+		}
+	}
+
+}
